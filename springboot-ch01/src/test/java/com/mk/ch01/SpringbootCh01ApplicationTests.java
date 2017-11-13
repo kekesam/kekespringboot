1287eb78f2f78b75f07243d73a80287c5988fa02
kekesam <xuchengfeifei@163.com>
2017-11-14 00:10:32 +0800

    springboot ch01

---
+++ springboot-ch01/src/test/java/com/mk/ch01/SpringbootCh01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mk.ch01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootCh01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
