256567318b6bc923c2da66bbb669fdc4b08d4231
kekesam <xuchengfeifei@163.com>
2017-11-24 23:26:02 +0800

    第四次提交!

--- springmvc-boot-ch4/src/main/java/com/mk/SpringmvcBootCh4Application.java
+++ springmvc-boot-ch4/src/main/java/com/mk/SpringmvcBootCh4Application.java
@@ -1,14 +1,38 @@
 package com.mk;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
 
 @SpringBootApplication
+@MapperScan(basePackages="com.mk.mapper")
 public class SpringmvcBootCh4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcBootCh4Application.class, args);
 	}
+	
+	/*@Bean
+    public Converter<String, Date> addNewConvert() {
+        return new Converter<String, Date>() {
+            @Override
+            public Date convert(String source) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = null;
+            try {
+                date = sdf.parse((String) source);
+                } catch (Exception e) {
+                e.printStackTrace();
+                }
+             return date;
+            }
+        };
+    }*/
 	
 //	@Bean
 //	public ObjectMapper getObjectMapper() {
--- springmvc-boot-ch4/src/test/java/com/mk/SpringmvcBootCh4ApplicationTests.java
+++ springmvc-boot-ch4/src/test/java/com/mk/SpringmvcBootCh4ApplicationTests.java
@@ -2,15 +2,26 @@ package com.mk;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mk.bean.User;
+import com.mk.service.IUserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringmvcBootCh4ApplicationTests {
 
+	@Autowired
+	private IUserService userService;
+	
 	@Test
 	public void contextLoads() {
+		User user = userService.getUser(1);
+		System.out.println(user.getId());
+		System.out.println(user.getAccount());
+		System.out.println(user.getUsername());
 	}
 
 }
